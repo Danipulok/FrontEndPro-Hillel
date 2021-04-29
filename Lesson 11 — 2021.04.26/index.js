4dc07e31ae219f2950cf0f7fe636bbf7f693a32f
Danipulok <danipulok@gmail.com>
2021-04-29 15:23:20 +0300

    Fixed linter

--- Lesson 11 — 2021.04.26/index.js
+++ Lesson 11 — 2021.04.26/index.js
@@ -1,33 +1,35 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-console */
 // Разобраться с правилами привязки this
 // Написали свою реализацию метода bind :)
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Example object
-let small = {
+const small = {
     a: 1,
-    go: function(b,c,d){
-        console.log("this.a: " + this.a + "; b: " + b + "; c: " + c + "; d:" + d);
-        console.log(this.a+b+c+d);
+    go: function (b, c, d) {
+        console.log('this.a: ' + this.a + '; b: ' + b + '; c: ' + c + '; d:' + d);
+        console.log(this.a + b + c + d);
     }
-}
+};
 
 
 // Standard bind
-console.log("[Standard bind]")
-let bindTest = small.go.bind({a: 500}, 5);
+console.log('[Standard bind]');
+const bindTest = small.go.bind({ a: 500 }, 5);
 bindTest(2, 3);
 
 
 // My option
 function myBind(target, boundThis, ...args) {
-    return function newFunc (...addArgs){
-        return target.call(boundThis, ...args, ...addArgs)
-    }
+    return function newFunc(...addArgs) {
+        return target.call(boundThis, ...args, ...addArgs);
+    };
 }
 
 
 // MyBind
-console.log("\n[Custom bind]")
-let myBindTest = myBind(small.go, {a: 500}, 5)
-myBindTest(2, 3)
+console.log('\n[Custom bind]');
+const myBindTest = myBind(small.go, { a: 500 }, 5);
+myBindTest(2, 3);

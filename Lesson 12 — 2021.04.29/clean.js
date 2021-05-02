2287089b90e8515d2b88a53452acd05fc9a80d11
Danipulok <danipulok@gmail.com>
2021-05-02 17:57:23 +0300

    Clean version uploaded

---
+++ Lesson 12 — 2021.04.29/clean.js
@@ -0,0 +1,44 @@
+function countModOrder(number) {
+    let mod = number % 10;
+    let order = 0;
+
+    while (number > 9) {
+        number = (number - number % 10) / 10
+        order += 1;
+    }
+    return [mod, order]
+}
+
+function reverseNumber(numberToReverse) {
+    let resultNumber = 0;
+
+    while(numberToReverse > 0) {
+        let mod, order;
+        [mod, order] = countModOrder(numberToReverse);
+        numberToReverse = (numberToReverse - numberToReverse % 10) / 10
+        resultNumber += (mod * 10 ** order);
+    }
+    return resultNumber;
+}
+
+function makePalindromeFromNumber(number) {
+    let steps = 0;
+
+    while(!(number === reverseNumber(number))){
+        number += reverseNumber(number);
+
+        if(++steps === 16){
+            steps = null;
+            number = null;
+            break;
+        }
+    }
+
+    return {
+        result: number,
+        steps: steps,
+    }
+}
+
+let res = makePalindromeFromNumber(178)
+console.log(res, "\n")
